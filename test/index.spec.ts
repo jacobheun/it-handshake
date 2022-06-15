ee7417dec41349b86c3c721de9a7456076ff9529
Alex Potsides <alex@achingbrain.net>
2022-06-15 11:46:55 +0100

    chore!: update deps (#20)
    Updates deps including it-pushable which now defaults to accepting `Uint8Array`s similar to node streams.
    
    BREAKING CHANGE: it-pushable types are exported as part of the public api and the default type isn't generic any more

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,4 +1,4 @@
-import { expect } from 'aegir/utils/chai.js'
+import { expect } from 'aegir/chai'
 import { handshake } from '../src/index.js'
 import { duplexPair } from 'it-pair/duplex'
 import { pipe } from 'it-pipe'
